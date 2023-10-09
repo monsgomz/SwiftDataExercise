f23b8ddfd23c7ef8164607f3e5c4fe8cbc2a0446
M Montserrat Gomez <montserrat.ga95@gmail.com>
2023-10-09 09:55:52 -0400

    Initial Commit

---
+++ SwiftDataExercise/SwiftDataExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataExerciseApp.swift
+//  SwiftDataExercise
+//
+//  Created by Montserrat Gomez on 2023-10-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
